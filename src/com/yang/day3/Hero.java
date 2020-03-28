a2d9a0a134c61d1a8f7bb1f9ca506d1e3a6a60b1
Administrator <2635681517@qq.com>
2020-03-28 23:11:59 +0800

    面向对象

--- src/com/yang/day3/Hero.java
+++ src/com/yang/day3/Hero.java
@@ -1,4 +1,23 @@
 package com.yang.day3;
 
 public class Hero {
+    String name;
+    float hp;
+    float armor;
+    int moveSpeed;
+
+    public static void main(String[] args) {
+        Hero hero = new Hero();
+        hero.name = "杨华钟";
+        hero.hp = 100.0f;
+        hero.armor = 50.0f;
+        hero.moveSpeed = 100;
+//创建两个对象
+        Hero heroo = new Hero();
+        heroo.name = "杨华钟o";
+        heroo.hp = 1000.0f;
+        heroo.armor = 500.0f;
+        heroo.moveSpeed = 1000;
+    }
+
 }
