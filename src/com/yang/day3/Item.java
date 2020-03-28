1a0018cc6416ebe449139c99c872a7a15d7d9cca
Administrator <2635681517@qq.com>
2020-03-28 23:07:56 +0800

    学习java

--- src/com/yang/day3/Item.java
+++ src/com/yang/day3/Item.java
@@ -4,4 +4,16 @@ public class Item {
     public String name;
     public int price;
 
+    public static void main(String[] args) {
+        Item item = new Item();
+        item.name = "雪萍";
+        item.price = 50;
+
+        item.name = "草鞋";
+        item.price = 300;
+
+        item.name = "长剑";
+        item.price = 150;
+    }
+
 }
