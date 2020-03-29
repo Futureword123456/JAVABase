9e75d79e930efb0b93dcf2ad10ebd5f9589d6b8a
Administrator <2635681517@qq.com>
2020-03-29 22:29:24 +0800

    表达式

--- src/com/yang/day4/HelloWorld4.java
+++ src/com/yang/day4/HelloWorld4.java
@@ -1,4 +1,15 @@
 package com.yang.day4;
 
 public class HelloWorld4 {
+    public static void main(String[] args) {
+        int i = 5;
+        System.out.println(5);
+    }
+
+    public void method1(final int j) {
+        final int i;
+        i = 5;
+        //i=6;
+        //j=10;
+    }
 }
