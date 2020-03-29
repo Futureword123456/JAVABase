c7065e86cdd5fda9a376d1f591c8b43962a3390c
Administrator <2635681517@qq.com>
2020-03-29 17:52:23 +0800

    字面值

--- src/com/yang/day4/HelloWorld1.java
+++ src/com/yang/day4/HelloWorld1.java
@@ -1,4 +1,39 @@
 package com.yang.day4;
 
 public class HelloWorld1 {
+    public static void main(String[] args) {
+        long val = 26L;
+        int dceVal = 26;
+        int oxVal = 032;
+        int hexVal = 0x1a;
+        int binVal = 0b11010;
+        float f1 = 123.4f;
+        double d1 = 123.4;
+        double d2 = 1234e2;
+        System.out.println(oxVal);
+        System.out.println(d1);
+        System.out.println(d2);
+
+        String name = "盖伦";
+        char a = 'c';
+
+        //以下是转义字符
+        char tab = '\t'; //制表符
+        char carriageReturn = '\r'; //回车
+        char newLine = '\n'; //换行
+        char doubleQuote = '\"'; //双引号
+        char singleQuote = '\''; //单引号
+        char backslash = '\\'; //反斜杠
+        //作业：
+        byte b = 12;
+        short s = 1;
+        int i = 256;
+        float f = 165;
+        double d = 515;
+        char c = '吃';
+        String str = "杨华钟";
+
+    }
+
+
 }
