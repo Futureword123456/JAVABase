022022152d8a123ff5f9416fc76bcd8495c7878e
Administrator <2635681517@qq.com>
2020-03-30 18:45:38 +0800

    表达式

---
+++ src/com/yang/day5/HelloWorld4.java
@@ -0,0 +1,13 @@
+package com.yang.day5;
+
+public class HelloWorld4 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i < 10; i++) {
+            double salary = Math.pow(2, (i - 1));
+            System.out.println("第十天收入是" + salary);
+        }
+
+
+    }
+}
