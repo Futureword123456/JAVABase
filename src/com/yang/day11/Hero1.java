32717503269df64b24be33617bfd185fd16faae5
Administrator <2635681517@qq.com>
2020-04-25 22:53:41 +0800

    枚举

---
+++ src/com/yang/day11/Hero1.java
@@ -0,0 +1,40 @@
+package com.yang.day11;
+
+public class Hero1 {
+    public static void main(String[] args) {
+        HeroType type = HeroType.ASSASSIN;
+
+        switch (type) {
+            case PUSH:
+                System.out.println("推进");
+                break;
+            case TANK:
+                System.out.println("坦克");
+                break;
+            case ASSIST:
+                System.out.println("辅助");
+                break;
+            case RANGED:
+                System.out.println("远程");
+                break;
+            case WIZARD:
+                System.out.println("法师");
+                break;
+            case FARMING:
+                System.out.println("打野");
+                break;
+            case WARRIOR:
+                System.out.println("近战");
+                break;
+            case ASSASSIN:
+                System.out.println("刺客");
+                break;
+        }
+        for (HeroType type1 : HeroType.values()) {
+            System.out.print(type1 + " ");
+
+        }
+
+    }
+
+}
