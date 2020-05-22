1682d0517f23178aeadd0bd98683025c6d636510
Administrator <2635681517@qq.com>
2020-05-22 13:20:12 +0800

    类

---
+++ src/com/yang/day19/Telphoto.java
@@ -0,0 +1,18 @@
+package com.yang.day19;
+
+public class Telphoto {
+    //属性（成员变量）
+    float screen;
+    float cpu;
+    float mem;
+
+    //方法
+    void call() {
+        System.out.println("Telphoto有打电话的功能!");
+    }
+
+    void sendMesage() {
+        System.out.println("Telphoto有发短信的功能!");
+    }
+
+}
