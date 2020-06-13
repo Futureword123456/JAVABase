089af8c200232a5fc29f2c52d224b2e6dc9a27ac
Administrator <2635681517@qq.com>
2020-06-13 12:10:16 +0800

    java基本学习学生选课管理

--- src/com/yang/day30/collection/MapTest.java
+++ src/com/yang/day30/collection/MapTest.java
@@ -22,6 +22,36 @@ public class MapTest {
         MapTest mapTest = new MapTest();
         mapTest.testPut();
         mapTest.testKeySet();
+//        mapTest.testRemove();
+////        mapTest.testEntrySet();
+        mapTest.testModify();
+        mapTest.testEntrySet();
+    }
+
+    /*
+    测试修改
+     */
+    public void testModify() {
+        System.out.println("请输入要修改的学生ID");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            //取得键盘输入的学生ID
+            String ID = scanner.next();
+
+            //根据i的查找学生id
+            Student student = students.get(ID);
+            if (student == null) {
+                System.out.println("学生id不存在，请重新输入");
+                continue;
+            }
+            System.out.println("当前改学生ID是对应学生是" + student.name);
+            System.out.println("请输入新的学生姓名");
+            String name = scanner.next();
+            Student student1 = new Student(ID, name);
+            students.put(ID, student1);
+            System.out.println("修改成功！！");
+            break;
+        }
     }
 
     /*
@@ -52,6 +82,38 @@ public class MapTest {
 
         }
 
+    }
+
+    /*
+    删除学生id
+     */
+    public void testRemove() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("请输入要删除的学生id");
+            String ID = scanner.next();
+            //判断id是否有对应的学生对象
+            Student st = students.get(ID);
+            if (st == null) {
+                System.out.println("ID不存在");
+                continue;
+            }
+            students.remove(ID);
+            System.out.println("成功删除学生" + st.name);
+            break;
+        }
+    }
+
+    /*
+    通过enterySet方法遍历MAP
+     */
+    public void testEntrySet() {
+        Set<Map.Entry<String, Student>> entrySet = students.entrySet();
+        for (Map.Entry<String, Student> entry : entrySet) {
+            System.out.println("取得键" + entry.getKey());
+            System.out.println("对应的值是" + entry.getValue().name);
+
+        }
     }
 
     public void testKeySet() {
